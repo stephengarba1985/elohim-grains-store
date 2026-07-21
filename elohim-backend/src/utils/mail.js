eb3cf523b0e9ca0b93379a5b2034ceac7c2ae122
Stephen Garba <stephengarba1985@gmail.com>
2026-07-21 23:43:27 +0100

    Add email environment variable debugging

--- elohim-backend/src/utils/mail.js
+++ elohim-backend/src/utils/mail.js
@@ -1,5 +1,10 @@
 const nodemailer = require("nodemailer");
 
+    // Debug - remove after testing
+console.log("EMAIL_HOST =", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT =", process.env.EMAIL_PORT);
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),

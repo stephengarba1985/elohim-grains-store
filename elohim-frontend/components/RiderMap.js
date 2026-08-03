8a871a4e2b59c27feaeccb055c09348f6a3f56b7
Stephen Garba <stephengarba1985@gmail.com>
2026-08-04 00:48:00 +0100

    Fix Leaflet marker asset 404s in rider map

--- elohim-frontend/components/RiderMap.js
+++ elohim-frontend/components/RiderMap.js
@@ -1,7 +1,16 @@
 "use client";
 
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+// Fix missing default marker assets in Next.js builds.
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 export default function RiderMap({ riders }) {
   return (
